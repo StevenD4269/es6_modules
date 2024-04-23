211beeb93e56283ac39796b83bf165aea7b6dee6
Steven Dickerson <poddocdan@gmail.com>
2024-04-23 07:15:23 -0700

    added default to class car

--- src/car.js
+++ src/car.js
@@ -1,4 +1,4 @@
-export class Car {
+export default class Car {
   constructor(id, make, model, year) {
     // The constructor method is a special method of a class for creating and initializing an object instance of that class.
     this.id = id; //sets each paramter to a property
